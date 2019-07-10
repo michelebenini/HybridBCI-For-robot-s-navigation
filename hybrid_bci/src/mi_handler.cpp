be24497fc4cabcfa67c5fcb8ade859c908ff4d61
michelebenini <michelebenini94@gmail.com>
2019-07-10 11:33:02 +0200

    viewer completed

---
+++ hybrid_bci/src/mi_handler.cpp
@@ -0,0 +1,11 @@
+#include "ros/ros.h"
+#include "hybrid_bci/motorimagery.h"
+#include "hybrid_bci/motorimagery_raw.h"
+
+int main(int argc, char **argv){
+  ros::init(argc, argv, "mi_handler");
+  ros::NodeHandle n;
+
+
+  return 0;
+}
---
+++ hybrid_bci/src/p300_handler.cpp
@@ -0,0 +1,13 @@
+#include "ros/ros.h"
+#include "hybrid_bci/P300.h"
+#include "hybrid_bci/P300_person.h"
+#include "hybrid_bci/P300_roi.h"
+#include "hybrid_bci/person_roi.h"
+
+int main(int argc, char **argv){
+  ros::init(argc, argv, "p300_handler");
+  ros::NodeHandle n;
+
+
+  return 0;
+}
